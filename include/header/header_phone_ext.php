8334ea6f72af53aeb35ceb9119575b1a89fa3e6a
VladProfitkit <vlad@profitkit.ru>
2024-11-14 05:06:17 +0400

    Подгрузка

--- include/header/header_phone_ext.php
+++ include/header/header_phone_ext.php
@@ -3,13 +3,7 @@
 		<div class="phones">
 			<div class="phones__phones-wrapper">
 				<div class="phones__inner phones__inner--big fill-theme-parent">
-                    <span class="icon-block__only-icon banner-light-icon-fill menu-light-icon-fill fill-theme-target">
-                        <i class="svg inline svg-inline-" aria-hidden="true"></i>
-                    </span>
-                    <span class="icon-block__icon banner-light-icon-fill menu-light-icon-fill">
-                        <i class="svg inline svg-inline-" aria-hidden="true"></i>
-                    </span>
-                    <a class="phones__phone-link phones__phone-first dark_link banner-light-text menu-light-text icon-block__name" href="tel:+7(812)-337-08-87">+7 (812) 223-60-94</a>
+ <span class="icon-block__only-icon banner-light-icon-fill menu-light-icon-fill fill-theme-target"> <i class="svg inline svg-inline-" aria-hidden="true"></i> </span> <span class="icon-block__icon banner-light-icon-fill menu-light-icon-fill"> <i class="svg inline svg-inline-" aria-hidden="true"></i> </span> <a class="phones__phone-link phones__phone-first dark_link banner-light-text menu-light-text icon-block__name" href="tel:+7(812)-337-08-87">+7 (812) 223-60-94</a>
 				</div>
 			</div>
 		</div>
